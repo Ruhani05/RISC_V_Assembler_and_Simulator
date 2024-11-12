8cee6ae1ca946e32a83ee135741e124f7c426143
Jaitrika <141649577+Jaitrika@users.noreply.github.com>
2024-11-12 10:33:38 +0530

    Update btype.py

--- SimpleSimulator/btype.py
+++ SimpleSimulator/btype.py
@@ -11,40 +11,52 @@ def binary_to_decimal_unsigned(binary):
     return decimal_value
 def beq(s,d,pc):
     if (binary_to_decimal(d[s[7:12]]))==(binary_to_decimal(d[s[12:17]])):
-        pc=pc+binary_to_decimal(s[0]+s[24]+s[1:7]+s[27:31]+'0')
+        pc=pc+binary_to_decimal(s[0]+s[24]+s[1:7]+s[20:24]+'0')
+    else:
+        pc=pc+4
     return pc
 def bne(s,d,pc):
     if (binary_to_decimal(d[s[7:12]]))!=(binary_to_decimal(d[s[12:17]])):
-        pc=pc+binary_to_decimal(s[0]+s[24]+s[1:7]+s[27:31]+'0')
+        pc=pc+binary_to_decimal(s[0]+s[24]+s[1:7]+s[20:24]+'0')
+    else:
+        pc=pc+4
     return pc
 def bge(s,d,pc):
-    if (binary_to_decimal(d[s[7:12]]))>=(binary_to_decimal(d[s[12:17]])):
-        pc=pc+binary_to_decimal(s[0]+s[24]+s[1:7]+s[27:31]+'0')
+    if (binary_to_decimal(d[s[7:12]]))<(binary_to_decimal(d[s[12:17]])):
+        pc=pc+binary_to_decimal(s[0]+s[24]+s[1:7]+s[20:24]+'0')
+    else:
+        pc=pc+4
     return pc
 def bgeu(s,d,pc):
-    if (binary_to_decimal_unsigned(d[s[7:12]]))>=(binary_to_decimal_unsigned(d[s[12:17]])):
-        pc=pc+binary_to_decimal(s[0]+s[24]+s[1:7]+s[27:31]+'0')
+    if (binary_to_decimal_unsigned(d[s[7:12]]))<(binary_to_decimal_unsigned(d[s[12:17]])):
+        pc=pc+binary_to_decimal(s[0]+s[24]+s[1:7]+s[20:24]+'0')
+    else:
+        pc=pc+4
     return pc
 def blt(s,d,pc):
-    if (binary_to_decimal(d[s[7:12]]))<(binary_to_decimal(d[s[12:17]])):
-        pc=pc+binary_to_decimal(s[0]+s[24]+s[1:7]+s[27:31]+'0')
+    if (binary_to_decimal(d[s[7:12]]))>(binary_to_decimal(d[s[12:17]])):
+        pc=pc+binary_to_decimal(s[0]+s[24]+s[1:7]+s[20:24]+'0')
+    else:
+        pc=pc+4
     return pc
 def bltu(s,d,pc):
-    if (binary_to_decimal_unsigned(d[s[7:12]]))<(binary_to_decimal_unsigned(d[s[12:17]])):
-        pc=pc+binary_to_decimal(s[0]+s[24]+s[1:7]+s[27:31]+'0')
+    if (binary_to_decimal_unsigned(d[s[7:12]]))>(binary_to_decimal_unsigned(d[s[12:17]])):
+        pc=pc+binary_to_decimal(s[0]+s[24]+s[1:7]+s[20:24]+'0')
+    else:
+        pc=pc+4
     return pc
 def check(s,d,pc):
     if s[17:20] == "000":
-        beq(s,d,pc)
+       return beq(s,d,pc)
     elif s[17:20] == "001":
-        bne(s,d,pc)
+       return bne(s,d,pc)
     elif s[17:20] == "100":
-        blt(s,d,pc)
+       return blt(s,d,pc)
     elif s[17:20] == "101":
-        bge(s,d,pc)
+       return bge(s,d,pc)
     elif s[17:20] == "110":
-        bltu(s,d,pc)
+       return bltu(s,d,pc)
     elif s[17:20] == "111":
-        bgeu(s,d,pc)
+       return bgeu(s,d,pc)
     else:
-        return
+        return pc
